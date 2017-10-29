bcbd0700cb97a7a19f87e2578d7a2114d5916fb7
bhupinderkumarbl <bhupinder.kumar@blinfosoft.com>
2017-10-29 13:04:38 +0530

    shipper page html

--- src/app/shipper/shipper.component.ts
+++ src/app/shipper/shipper.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 
 @Component({
-    template: '<h1>Shipper</h1>'
+    templateUrl: 'shipper.html',
+    styleUrls: ['shipper.css']
+
+
 })
 export class ShipperComponent {}
