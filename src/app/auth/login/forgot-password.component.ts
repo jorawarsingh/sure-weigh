d06730e0e2b5d539646489fc54d321f320b62fba
bhupinderkumarbl <bhupinder.kumar@blinfosoft.com>
2017-10-29 12:12:09 +0530

    css changes

--- src/app/auth/login/forgot-password.component.ts
+++ src/app/auth/login/forgot-password.component.ts
@@ -1,14 +1,14 @@
 import {Component} from '@angular/core';
 
 @Component({
-  template: `<div class="login">
+    template: `
                 <form>
-                  <label>Enter the associated with your account and we'll send you link to reset your password</label>
+                  <label class="marginThirty">Enter the associated with your account and we'll send you link to reset your password</label>
                   <input type="text" placeholder="name@example.com">
                   <span>Error message</span>
                   <button type="button">Send Reset<br> Password Link</button>
                 </form>
-              </div>`
+              `
 })
 export class ResetPasswordComponent {
 }
